d7245446efd9bd300030c7c2561ec88fa1fb07a8
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2023-12-21 10:11:12 -0400

    Modo Oscuro y Claro Boton Implementado

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,22 @@ window.Alpine = Alpine;
 Alpine.plugin(focus);
 
 Alpine.start();
+
+if (localStorage.dark == 1) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
+
+
+document.querySelectorAll(".setMode").forEach(item =>
+    item.addEventListener("click", () => {
+            if (localStorage.dark == 1) {
+                localStorage.dark = 0;
+                document.documentElement.classList.remove('dark');
+            } else {
+                localStorage.dark = 1;
+                document.documentElement.classList.add('dark');
+            }
+        })
+    )
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -76,6 +76,16 @@
                     </x-dropdown>
                     @endrole
 
+              <!-- Selector de tema oscuro -->
+                <div class="mr-2">
+                    <svg id="moon" class="setMode h-4 w-4 text-gray-400 hover:text-gray-500 cursor-pointer" fill="none" @click="toggle" :class="{'block': !show, 'hidden': show}" x-cloak xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                    </svg>
+                    <svg id="sun" class="setMode h-5 w-5 text-yellow-200 hover:text-yellow-300 cursor-pointer" fill="none" @click="toggle" :class="{'hidden': !show, 'block': show}" x-cloak xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                </div>
+
 
 
 
